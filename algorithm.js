1b93c7a66d27d6fc20e144f06ce9a124815250a2
Fung <17ecf3@queensu.ca>
2021-01-01 19:50:32 -0500

    more concise code, still runs O(n^2) I think

--- algorithm.js
+++ algorithm.js
@@ -1,10 +1,14 @@
 function isEqual(a, b) {
     // if length is not equal
-    for (let i = 0; i < a.length; i++)
-        for (let j = 0; j < b.length; j++)
-            if (a[i] == b[j])
-                return False;
-    return True;
+    return a.some(value => b.includes(value))
 }
 var v = isEqual();
-document.write(v); 
+document.write(v);
+
+
+//legacy code, O(n^2)
+// for (let i = 0; i < a.length; i++)
+    //     for (let j = 0; j < b.length; j++)
+    //         if (a[i] == b[j])
+    //             return False;
+    // return True;
